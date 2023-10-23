f9830f0410072a5797cced6172dfb20a8be706ac
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2023-10-23 15:27:56 +0200

    Added Naviagtion to Layout

---
+++ src/components/ui/Nav/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <div  className="bg-primary">
+            <h1>VenueVista</h1>
+        </div>
+    )
+}
+
+export default Navbar;
