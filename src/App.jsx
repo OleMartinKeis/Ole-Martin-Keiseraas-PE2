af74f6aca3c557c816a25642cd3fa359c86eea61
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2023-10-24 16:45:38 +0200

    Added Register and Login Paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import Home from './components/pages/home'
+import Login from './components/pages/login'
 import Profile from './components/pages/profile'
+import Register from './components/pages/register'
 import Layout from './components/ui/Layout'
 import './index.css'
 import { Routes, Route } from 'react-router'
@@ -14,6 +16,8 @@ function App() {
                     <Route path="/" element={<Home />} />
                     <Route path="home" element={<Home />} />
                     <Route path="/profile" element={<Profile />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/login" element={<Login />} />
                 </Routes>
         </Layout>
     </div>
