1a7814d232826947ee174887c1cf0eeae74e9f12
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2023-11-03 13:11:48 +0100

    Fixed footer

--- src/components/ui/Layout/Footer/index.jsx
+++ src/components/ui/Layout/Footer/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Footer() {
-    return <footer  className="bg-primary bottom-0 fixed p-2 w-screen">This is my footer</footer>
+    return <footer  className="bg-primary bottom-0 p-2 w-screen">This is my footer</footer>
 }
 
 export default Footer;
