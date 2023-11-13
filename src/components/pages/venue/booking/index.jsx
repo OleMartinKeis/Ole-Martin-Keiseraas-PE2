a0d8547791350ede0237e613d3dcd4a4ba49485f
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2023-11-13 10:01:35 +0100

    Created calendar with bookings

--- src/components/pages/venue/booking/index.jsx
+++ src/components/pages/venue/booking/index.jsx
@@ -1,71 +1,23 @@
 import React, { useEffect, useState } from "react";
-import useAPI from "../../../storage/getApi";
 import { useParams } from "react-router-dom";
 import { API_HOST_URL } from "../../../storage/constants";
 import { useAuth } from "../../../storage/authentication";
 
-function Booking() {
-    const { id } = useParams();
-    const [bookings, setBookings] = useState([]);
-    const [loading, setLoading] = useState(true)
-    const { isAuthenticated } = useAuth();
-    const authToken = isAuthenticated ? localStorage.getItem("token") : null;
-
-    useEffect(() => {
-        if (!authToken) {
-            setLoading(false);
-            return;
-        }
-
-        const fetchBookings = async () =>{
-
-            try {
-                
-                const headers = {
-                    "Content-Type": "application/json",
-                    "Authorization": `Bearer ${authToken}`,
-                };
-
-                console.log(authToken)
-                console.log(headers)
-                
-                const response = await fetch(`${API_HOST_URL}/bookings`, headers);
-                console.log(response)
-                if (response.ok) {
-                    const data = await response.json();
-                    setBookings(data)
-                    
-                } else {
-                    console.error("Failed to fetch bookings")
-                }
-            } catch (error) {
-                console.error("Error:", error)
-            } finally {
-                setLoading(false)
-            }
-        };
-        fetchBookings();
-    }, [authToken])
-
-    if (loading) {
-        return <div>Loading...</div>
-    }
-    return (
+const BookingInfo = ({ booking }) => (
+    
         <div>
-            <h2>Bookings for this Venue</h2>
-            <ul>
-            {bookings.map((booking) => (
-                <li key={booking.id}>
+            <li key={booking.id}>
                 Booking ID: {booking.id}
                 <br />
                 Date From: {booking.dateFrom}
                 <br />
                 Date To: {booking.dateTo}
-                </li>
-            ))}
-            </ul>
+            </li>
       </div>
-    )
-}
+    
+)
+    
 
-export default Booking;
+
+
+export default BookingInfo;
--- src/components/pages/venue/index.jsx
+++ src/components/pages/venue/index.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import useAPI from "../../storage/getApi";
 import { API_HOST_URL } from "../../storage/constants";
 import Booking from "./booking";
-
+import BookingInfo from "./booking";
+import Calendar from "react-calendar";
 
 function Venue() {
     const { id } = useParams();
-    const { data } = useAPI(`${API_HOST_URL}/venues/${id}?owner=true&bookings=true`)
+    const { data } = useAPI(`${API_HOST_URL}/venues/${id}?_owner=true&_bookings=true`);
+    const [selectedDate, setSelectedDate] = useState(null);
     
     if (!data) {
         return <div>Loading...</div>
@@ -19,6 +21,18 @@ function Venue() {
         breakfast: "fa-mug-hot",
         parking: "fa-square-parking",
     };
+
+    const bookedDates = data.bookings && data.bookings.length > 0
+    ? data.bookings.map((booking) => ({
+        dateFrom: booking.dateFrom,
+        dateTo: booking.dateTo,
+        title: `Booking ID: ${booking.id}`,
+      }))
+    : [];
+
+    const handleDateChange = (date) => {
+        setSelectedDate(date);
+    }
 
     
     return(
@@ -82,13 +96,37 @@ function Venue() {
                         <h3>Anonymous</h3>
                     )}
                 </div>
-                {data.bookings &&
-                    data.bookings.map(() => (
-                        <div>{data.booking}</div>
-                    ))}
-                <Booking />
+            <div>
+            {data.bookings && data.bookings.length > 0 && (
+                <div>
+                    <h2>Bookings for this Venue</h2>
+                    <ul>
+                        {data.bookings.map((booking) => (
+                        <BookingInfo key={booking.id} booking={booking} />
+                        ))}
+                    </ul>
+                    <h2>Calendar</h2>
+                    <Calendar
+                    onChange={handleDateChange}
+                    value={selectedDate}
+                    tileClassName={({ date, view }) => {
+                        if (view === "month") {
+                        const isBooked = bookedDates.some(
+                            (bookingDate) =>
+                            date >= new Date(bookingDate.dateFrom) && date <= new Date(bookingDate.dateTo)
+                        );
+                    return isBooked && "bg-background text-grey border-radius-1 rounded-md ";
+                }
+                return null;
+                }}
+                className="bg-white text-text mt-6 p-4 border border-gray-300 rounded-lg shadow-md max-w-md mx-auto"
+
+                />
+                </div>
+                )}
             </div>
         </div>
+    </div>
     )
     
 }
