29c75595a436d69c7f8e46ed99d35748542604db
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2023-11-16 09:09:29 +0100

    Changed main class

--- src/components/ui/Layout/index.jsx
+++ src/components/ui/Layout/index.jsx
@@ -5,7 +5,7 @@ import Footer from "./Footer";
 function Layout({ children }) {
     return <>
             <Header />
-            <main className="bg-background text-white min-h-screen">
+            <main className="bg-background text-white min-h-screen min-w-screen">
                 {children}
             </main>
             <Footer />
