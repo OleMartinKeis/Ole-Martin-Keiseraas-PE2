a119c0ababe4812c3fd2c2d2fed0f484810c32d0
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2023-11-19 11:57:44 +0100

    Added styling to Login

--- src/components/pages/login/index.jsx
+++ src/components/pages/login/index.jsx
@@ -53,20 +53,36 @@ function Login() {
     }
 
     return (
-        <div>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div>
-                    <label>Email:</label>
-                    <Controller name="email" control={control} rules={{ required: 'Name is required' }} render={({ field }) => <input className="bg-background border-b border-accent ml-3 px-2 leading-tight" {...field} required type="text" />}/>
+        <div className="flex flex-col justify-center items-center content-center h-screen">
+            <div className="flex flex-col items-center">
+                <form onSubmit={handleSubmit(onSubmit)} className="flex flex-row mb-8">
+                    <div className="flex mb-2">
+                        <label className="pr-2">Email:</label>
+                        <Controller name="email" control={control} rules={{ required: 'Name is required' }} render={({ field }) => <input className="bg-background border-b border-accent pr-4 w-full leading-tight" {...field} required type="text" />}/>
+                    </div>
+                    <div className="flex mb-2">
+                        <label className="ml-2 w-20">Password:</label>
+                        <Controller name="password" control={control} rules={{ required: 'Name is required' }} render={({ field }) => <input {...field} className="bg-background border-b border-accent px-2 leading-tight" type="text" required/>}/>
+                    </div>
+                    <div>
+                        <button disabled={isSubmitting}  className="ml-4 w-full bg-cta p-1 rounded-md" type="Submit">Submit</button>
+                    </div>
+                </form>
+                <div className="flex flex-col mt-8">
+                    <p className="mb-2">Still not a member, or want to create a Venue Manager* account? <a className="border-accent border-b text-gray-200" href="/register">Click here to register</a><span className="opacity-50 text-sm ml-2">*To rent out a venue, you need a Venue Manager account</span></p>
                 </div>
-                <div>
-                    <label>Password:</label>
-                    <Controller name="password" control={control} rules={{ required: 'Name is required' }} render={({ field }) => <input {...field} className="bg-background border-b border-accent ml-3 px-2 leading-tight" type="text" required/>}/>
+            </div>
+            <div className="ml-1 mt-8 w-100">
+                <h2 className="text-2xl font-['Playfair_Display_SC']mb-4">FAQ</h2>
+                <div className="border-accent border-b w-100 p-4 mb-4">
+                <h3 className="text-xl">What is VenueVista?</h3>
+                <p className="px-2">VenueVista is a school project for Noroffs Exam for Front-end Developers</p>
                 </div>
-                <div>
-                    <button disabled={isSubmitting}  className="bg-cta" type="Submit">Submit</button>
+                <div className="border-b border-accent w-100 p-4">
+                <h3 className="text-xl">Who created VenueVista?</h3>
+                <p className="px-2">VenueVista is created by Ole Martin Keiseraas, check out my <strong><a href="https://github.com/OleMartinKeis">Github</a></strong></p>
                 </div>
-            </form>
+            </div>
         </div>
     )
 }
