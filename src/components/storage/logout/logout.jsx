69a4e7b1a09570a3af899651af020b53b1e355f2
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2023-11-22 15:18:38 +0100

    Created logout function

--- src/components/storage/logout/logout.jsx
+++ src/components/storage/logout/logout.jsx
@@ -2,16 +2,21 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 function Logout() {
-    const navigate = useNavigate();
-    const handleButtonClick = () =>{
-        localStorage.removeItem("user");
-        localStorage.removeItem("token");
-        navigate("/home");
-    }   
-  
-    return(
-        <button className="bg-cta" onClick={handleButtonClick}>Log out</button>
-    )
+  const navigate = useNavigate();
+  const handleButtonClick = () => {
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+    navigate("/home");
+  };
+
+  return (
+    <button
+      className="rounded-md mt-2 bg-cta text-white text-sm p-1"
+      onClick={handleButtonClick}
+    >
+      Log out
+    </button>
+  );
 }
 
 export default Logout;
