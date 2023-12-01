ef9e70c4a03342b74d0d36be097257a9dc81599d
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2023-12-01 09:43:16 +0100

    Updated footer

--- src/components/ui/Layout/Footer/index.jsx
+++ src/components/ui/Layout/Footer/index.jsx
@@ -2,8 +2,35 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="bg-primary bottom-0 pt-2 pb-2 w-100">
-      This is my footer
+    <footer className="bg-primary bottom-0 pt-4 pb-4 w-100">
+      <div className="grid md:grid-cols-2">
+        <div>
+          <p className="text-sm md:text-base">
+            This is not a real venue booking page. <br></br>This is a task
+            created for Noroff by
+            <a
+              href="https://github.com/OleMartinKeis"
+              className="border-b border-accent hover:text-accent font-bold ml-1"
+            >
+              Ole Martin Keiseraas
+            </a>
+            .
+          </p>
+        </div>
+        <div className="">
+          <p className="text-sm md:text-base">
+            All rights reserved &copy; <strong> 2023 VenueVista</strong>.
+          </p>
+          <div>
+            <a href="https://github.com/OleMartinKeis">
+              <i className="fa fa-github text-xl sm:text-3xl"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/ole-martin-keiseraas-615a19152/">
+              <i className="fa fa-linkedin-square text-xl sm:text-3xl ml-3"></i>
+            </a>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 }
